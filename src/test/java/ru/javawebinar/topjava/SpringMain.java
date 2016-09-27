c31b3ee061e6d952bc1729a2e5fe58a4d5802b83
DenisAbramov <den.abramov.89@inbox.ru>
2016-09-27 21:54:08 +0500

    3_7 unit4

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.web.user.AdminRestController;
+
 import java.util.Arrays;
 /**
  * User: gkislin
@@ -23,8 +25,11 @@ public class SpringMain
                         LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
         filteredMealsWithExceeded.forEach(System.out::println);*/
         // java 7 Automatic resource management
+
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml"))
         {
+            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            adminUserController.create(UserTestData.USER);
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
           //  AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
           //  adminUserController.create(new User(1, "userName", "email", "password", Role.ROLE_ADMIN));
