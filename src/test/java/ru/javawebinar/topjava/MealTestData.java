71aa5a23608293fac51350c46afc1a68149ba1f7
DenisAbramov <den.abramov.89@inbox.ru>
2016-10-03 20:52:36 +0500

    Третье домашнее задание

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -2,13 +2,36 @@ package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 /**
  * GKislin
  * 13.03.2015.
  */
-public class MealTestData {
+public class MealTestData
+{
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final int MEAL_ID = START_SEQ + 2;
+
+    public static final Meal MEAL_1 = new Meal(100002, LocalDateTime.now(), "Завтрак", 2000);
+    public static final Meal MEAL_2 = new Meal(100003, LocalDateTime.now(), "Ужин", 3000);
+
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                    (Objects.equals(expected.getCalories(), actual.getCalories())
+                            && Objects.equals(expected.getId(), actual.getId())
+                            && Objects.equals(expected.getDateTime(), actual.getDateTime())
+                            && Objects.equals(expected.getCalories(), actual.getCalories())
+                    )
+    );
 
 }
