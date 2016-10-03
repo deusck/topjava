7c09f6fb66902c2ab06d5fb74298985fd57c60c4
DenisAbramov <den.abramov.89@inbox.ru>
2016-10-03 20:59:32 +0500

    Четвертое12

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -10,11 +10,15 @@ import java.time.LocalTime;
  */
 public class Meal extends BaseEntity {
 
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    private final String description;
+    private String description;
 
-    private final int calories;
+    private int calories;
+
+    public Meal()
+    {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories)
     {
@@ -62,6 +66,18 @@ public class Meal extends BaseEntity {
     public LocalTime getTime()
     {
         return dateTime.toLocalTime();
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
     }
 
     public boolean isNew()
