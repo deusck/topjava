ae020ef5d4cf935ad47b521ef5e7666854ef1e51
DenisAbramov <den.abramov.89@inbox.ru>
2016-10-05 21:26:19 +0500

    6

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -16,6 +18,8 @@ public class Meal extends BaseEntity {
 
     private int calories;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
     public Meal()
     {
     }
@@ -78,6 +82,14 @@ public class Meal extends BaseEntity {
 
     public void setCalories(int calories) {
         this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public boolean isNew()
